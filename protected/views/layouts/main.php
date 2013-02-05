b38bca5c02ce34530f1139f1c381cd654d633c39
Alexander <Alexander@192.168.100.54>
2013-02-05 11:16:33 +0100

    added; documents outbound model and crud documents inbound model and crud log model and crud

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,9 +29,9 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Documents Inbound', 'url'=>array('/documentsinbound')),
+				array('label'=>'Documents Outbound', 'url'=>array('/documentsoutbound')),
+				array('label'=>'Log', 'url'=>array('/log')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
